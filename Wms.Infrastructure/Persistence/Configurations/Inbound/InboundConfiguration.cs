5f4b3d064db6bc3719d5ec352883acb67eb6e1e6
ducthinh3727 <vuducthinh33hihi@gmail.com>
2026-05-15 13:01:19 +0700

    up v0

--- Wms.Infrastructure/Persistence/Configurations/Inbound/InboundConfiguration.cs
+++ Wms.Infrastructure/Persistence/Configurations/Inbound/InboundConfiguration.cs
@@ -111,7 +111,7 @@ namespace Wms.Infrastructure.Persistence.Configurations.Inbound
             builder.Property(x => x.Quantity)
                    .IsRequired();
 
-            builder.Property(x => x.InboundOrderItemId);
+            builder.Property(x => x.InboundOrderItemId).HasColumnName("POIid");
 
             builder.Property(x => x.CreatedAt)
                    .IsRequired();
